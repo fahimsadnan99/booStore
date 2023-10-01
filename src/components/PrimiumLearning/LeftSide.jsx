30b1a0a85eb7e93669ccdbd1b358e2132feafc66
unknown <fahimgithub@gmail.com>
2023-10-01 19:43:20 +0600

    add

--- src/components/PrimiumLearning/LeftSide.jsx
+++ src/components/PrimiumLearning/LeftSide.jsx
@@ -4,6 +4,7 @@ import primiumObj from "../../assets/PremiumObj.svg"
 const leftSide = () => {
   return (
     <div>
+      
         <img src={primiumObj} className='w-80 py-16' alt='premiumObject'/>
     </div>
   )
